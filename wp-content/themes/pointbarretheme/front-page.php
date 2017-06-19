728491d4e1b46a09efe7c0e546cb0466847f2958
olivier <opoirot@yahoo.fr>
2017-06-19 17:41:27 +0200

    Container projet bof

--- wp-content/themes/pointbarretheme/front-page.php
+++ wp-content/themes/pointbarretheme/front-page.php
@@ -5,14 +5,19 @@
 		<h2>Projets swag</h2>
 		<?php
 		$query = new WP_Query(array("post_type" => "Projets", "tag" => "swag"));
-		while ($query->have_posts()) : $query->the_post(); ?>
+		while ($query->have_posts()) : $query->the_post();
+		 ?>
 			<h3><a class="openproject" data-permalink="<?php the_permalink(); ?> "href="#"> <?php the_title(); ?></a></h3>
 		<?php endwhile; ?>
 
 		<h2>Tous les projets</h2>
 		<?php
 		$query = new WP_Query(array("post_type" => "Projets"));
-		while ($query->have_posts()) : $query->the_post(); ?>
+		while ($query->have_posts()) : $query->the_post();
+		echo get_the_post_thumbnail($id, 'thumbnail', array('class' => 'alignleft')); 
+
+		?>
+
 			<h3><a class="openproject" data-permalink="<?php the_permalink(); ?> "href="#"> <?php the_title(); ?></a></h3>
 		<?php endwhile; ?>
 	</div>
@@ -22,6 +27,25 @@
 			<p id="quote">Ici va apparaître la quote aléatoire parmi une liste gérée dans le back.</p>
 		</div>
 		<div id="prescontainer"></div>
+	</div>
+
+	<div id="projetscontainer">
+			<?php
+				$query = new WP_Query(array("post_type" => "Projets"));
+					while ($query->have_posts()) : $query->the_post();
+				echo get_the_post_thumbnail($id, 'thumbnail'); 
+			?>
+
+<div class="projetsolo">
+			<img src=" <?php echo get_the_post_thumbnail($id, 'thumbnail');  ?> ">
+			<h3>
+			<a class="openproject" data-permalink="<?php the_permalink(); ?> "href="#">
+				<?php the_title(); ?>
+			</a>
+		</h3>
+</div>
+					<?php endwhile; ?>
+		
 	</div>
 
 	<div class="fullscreenproject"> </div>
--- wp-content/themes/pointbarretheme/js/script.js
+++ wp-content/themes/pointbarretheme/js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function()
 		$(".fullscreenproject").css('display', 'inline');
 		$(".fullscreenproject").animate(
 			{
-				'opacity' : '1'
+				'opacity' : '0'
 			}, 250);
 		return false;
 	});
@@ -42,7 +42,7 @@ $(document).ready(function()
 			$(".fullscreenproject").css('display', 'inline');
 			$(".fullscreenproject").animate(
 				{
-					'opacity' : '1'
+					'opacity' : '0'
 				}, 250);
 		}
 		else
