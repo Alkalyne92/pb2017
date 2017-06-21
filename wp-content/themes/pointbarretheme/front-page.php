e4cffe1e24fdb58a5e7ee6b3bd30a6caaaa1e4b0
Aleqsandr <alexandre.reho@gmail.com>
2017-06-21 17:41:37 +0200

    problème de font

--- wp-content/themes/pointbarretheme/front-page.php
+++ wp-content/themes/pointbarretheme/front-page.php
@@ -39,10 +39,7 @@
 					if ($projetsolo->have_posts()) : while ($projetsolo->have_posts()) : $projetsolo->the_post();
 					?>					
 
-			<div class="projetsolo">
-				<figure>
-					<img src="<?php the_post_thumbnail_url(''); ?>" />
-				</figure>
+			<div class="projetsolo" style="background-image: url(<?php the_post_thumbnail_url('') ?>)">
 					<div class="encart" style="background-color:<?php echo get_post_meta( $post->ID, 'Couleur Encart', true ); ?> ;">
 
 							<h2><?php the_title(); ?></h2>
