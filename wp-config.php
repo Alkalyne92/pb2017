4001d211262b51e570efefd331ee64572c122f41
Aleqsandr <alexandre.reho@gmail.com>
2017-06-22 17:13:45 +0200

    Ajout du plugin Order Wordpress

--- wp-config.php
+++ wp-config.php
@@ -19,13 +19,13 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'pb2017');
+define('DB_NAME', 'dev_pb2017');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'root');
+define('DB_USER', 'merci');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'JqByZwrjnZrh5u28');
 
 /** Adresse de l’hébergement MySQL. */
 define('DB_HOST', 'localhost');
