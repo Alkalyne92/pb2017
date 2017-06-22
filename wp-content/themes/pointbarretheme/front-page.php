74350197d394ee32a118d257a10f479380af2bac
Alkalyne92 <lea.mrtte@gmail.com>
2017-06-22 16:06:32 +0200

    Taille encart

--- wp-content/themes/pointbarretheme/front-page.php
+++ wp-content/themes/pointbarretheme/front-page.php
@@ -47,10 +47,17 @@
 					<div class="titleoverlay" style="background-color: <?php echo get_post_meta( $post->ID, 'Couleur Encart', true); ?>"></div>
 				</div>
 				<div class="projetsolo" style="background-image: url(<?php the_post_thumbnail_url('') ?>)">
+								
+				<div class="encartcontainer">
+
 					<div class="encart" style="background-color:<?php echo get_post_meta( $post->ID, 'Couleur Encart', true ); ?> ;">							
 						<p><?php the_category(); ?></p>
 						<h4></h4>
 					</div>
+				
+					<div class="encartoverlay"></div>
+					
+				</div>	
 					<div class="projetsolooverlay"></div>
 				</div> <!-- projet solo fin-->
 			</div>
